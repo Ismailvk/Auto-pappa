c6a3c2c2b99efa7dea124e4d56562184b32f10df
Ismailvk <ismailkbassi00@gmail.com>
2024-06-12 20:41:42 +0530

    updated the ui

--- lib/views/vehicle_deails_screen.dart
+++ lib/views/vehicle_deails_screen.dart
@@ -4,10 +4,16 @@ import 'package:auto_pappa/resources/constants/app_colors.dart';
 import 'package:auto_pappa/resources/constants/image_urls.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class VehicleDetailsScreen extends StatelessWidget {
-  VehicleDetailsScreen({super.key});
+class VehicleDetailsScreen extends StatefulWidget {
+  const VehicleDetailsScreen({super.key});
 
+  @override
+  State<VehicleDetailsScreen> createState() => _VehicleDetailsScreenState();
+}
+
+class _VehicleDetailsScreenState extends State<VehicleDetailsScreen> {
   List<FeaturedServices> featuredServicesList = [
     FeaturedServices(
         imagePath: ImageUrls.interiorWashing, title: 'Interior Washing'),
@@ -17,8 +23,11 @@ class VehicleDetailsScreen extends StatelessWidget {
     FeaturedServices(imagePath: ImageUrls.tyreWashing, title: 'Body Washing'),
   ];
 
+  int activeIndex = 0;
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: AppColors.white,
@@ -29,27 +38,52 @@ class VehicleDetailsScreen extends StatelessWidget {
           Padding(padding: EdgeInsets.all(10), child: Icon(Icons.more_vert))
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child: CarouselSlider(
-          items: featuredServicesList
-              .map((e) => ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Image(
-                      image: AssetImage(e.imagePath),
-                      fit: BoxFit.cover,
-                      width: double.infinity,
-                    ),
-                  ))
-              .toList(),
-          options: CarouselOptions(
-            aspectRatio: 16 / 10,
-            viewportFraction: 0.98,
-            enableInfiniteScroll: true,
-            autoPlayCurve: Curves.fastOutSlowIn,
-            enlargeCenterPage: true,
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: CarouselSlider(
+                  items: featuredServicesList
+                      .map((e) => ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image(
+                              image: AssetImage(e.imagePath),
+                              fit: BoxFit.cover,
+                              width: double.infinity,
+                            ),
+                          ))
+                      .toList(),
+                  options: CarouselOptions(
+                    onPageChanged: (index, reason) =>
+                        setState(() => activeIndex = index),
+                    aspectRatio: 16 / 10,
+                    viewportFraction: 0.98,
+                    enableInfiniteScroll: false,
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    enlargeCenterPage: true,
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                left: size.width / 2.5,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                  child: AnimatedSmoothIndicator(
+                      effect: WormEffect(
+                          activeDotColor: AppColors.primaryColor,
+                          dotHeight: 7,
+                          dotWidth: 7),
+                      activeIndex: activeIndex,
+                      count: featuredServicesList.length),
+                ),
+              )
+            ],
           ),
-        ),
+        ],
       ),
     );
   }
